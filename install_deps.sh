32b784906e1229494bd3a2f05c44cf3748423808
David Norton <dgnorton@gmail.com>
2015-04-03 11:52:24 -0400

    add script to instal deps

---
+++ install_deps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# exit on error
+set -e
+
+# install needed packages
+apt-get update && apt-get install -y mercurial bzr
+
+# download Dave Cheney's (unofficial) ARM build of golang compiler / tools
+curl -o go.tar.gz http://dave.cheney.net/paste/go1.4.2.linux-arm~armv5-1.tar.gz
+
+# install golang
+tar -C /usr/local -xzf go.tar.gz
+
+# add environment vars for golang
+printf "\n" >> $HOME/.bashrc
+printf "export PATH=$PATH:/usr/local/go/bin\n" >> $HOME/.bashrc
+printf "export GOPATH=$HOME/go\n" >> $HOME/.bashrc
+
+source . $HOME/.bashrc
