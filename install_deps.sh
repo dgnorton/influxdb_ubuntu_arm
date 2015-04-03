807532a1b0954c27e622fcfdc3140a0ff986b058
David Norton <dgnorton@gmail.com>
2015-04-03 11:59:52 -0400

    added git to install

--- install_deps.sh
+++ install_deps.sh
@@ -4,7 +4,7 @@
 set -e
 
 # install needed packages
-apt-get update && apt-get install -y mercurial bzr
+apt-get update && apt-get install -y git mercurial bzr
 
 # download Dave Cheney's (unofficial) ARM build of golang compiler / tools
 curl -o go.tar.gz http://dave.cheney.net/paste/go1.4.2.linux-arm~armv5-1.tar.gz
